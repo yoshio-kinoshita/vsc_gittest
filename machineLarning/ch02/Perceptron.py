6c9dfa98da4f22dcbfae2774af40e1fa22bb262d
木下 善雄 <kinoshita.yoshio@sg.itcc.co.jp>
2017-03-28 18:58:27 +0900

    commit

---
+++ machineLarning/ch02/Perceptron.py
@@ -0,0 +1,29 @@
+import numpy as np
+
+
+class Perceptron(object):
+    """ パーセプトロンの分類機
+
+    パラメータ
+    eta : float
+        学習率(0.0より大きく1.0以下の値)
+    
+    n_iter : int
+        トレーニングデータのトレーニング回数
+
+    属性
+    ---------------------------
+    w_ :  1次元配列
+        適合後の重み
+
+    errors_ : リスト
+        各エポックでの誤分類機
+    """
+
+    def __init__(self, eta=0.01, n_iter=10):
+        self.eta = eta
+        self.n_iter = n_iter
+
+    
+    def fit(self, x, y):
+        """ トレーニングデータに適合させる
